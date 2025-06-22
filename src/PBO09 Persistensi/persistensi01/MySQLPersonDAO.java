ac20eecd31e5ece64f20a8bb572f215f8045d9df
Taki <syafiqabiyyutaqi123@gmail.com>
2025-06-22 23:55:42 +0700

    GUI 13b

--- src/PBO09 Persistensi/persistensi01/MySQLPersonDAO.java
+++ src/PBO09 Persistensi/persistensi01/MySQLPersonDAO.java
@@ -4,17 +4,19 @@
 /* Pembuat		: Syafiq Abiyyu Taqi / 24060123120012 */
 /* Tanggal		: 15-05-2025 */
 /***********************************/
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class MySQLPersonDAO implements PersonDAO{
     public void savePerson(Person person) throws Exception {
         String name = person.getName();
         // membuat koneksi, nama db, user, password menyesuaikan
-        Class.forName("com.mysql.jdbo.Driver");
+        Class.forName("com.mysql.jdbc.Driver");
         Connection con = DriverManager.getConnection(
             "jdbc:mysql://localhost:3306/PBO",
             "root",
-            "8604biu157"
+            "taki157"
         );
         String query = "INSERT INTO person (name) VALUES ('" + name + "')";
         System.out.println(query);
